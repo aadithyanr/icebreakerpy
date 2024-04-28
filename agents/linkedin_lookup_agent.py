3bd622ebcce480fedde70a5307d86823b6cafbfc
Aadithyan <nair.aadithyan999@gmail.com>
2024-04-28 21:59:13 +0400

    learning tools

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -4,4 +4,10 @@ from langchain.agents import initialize_agent, Tool, AgentType
 
 
 def lookup(name: str) -> str:
-    return "Linkedin Profile URL"
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    template = """
+    given the full name {name_of_person}, i want you to get me a link of that person's linkedin page. just reply back with only a url. no text pls.
+    """
+    tools_for_agent = [Tool(name="crawl google 4 linkedin profile page", func = "", description="useful to get linkedin urls")]
+    
+    
