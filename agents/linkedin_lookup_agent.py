873637efa29b5edef0c82c42cf44e5f17989356e
Aadithyan <nair.aadithyan999@gmail.com>
2024-04-28 21:58:58 +0400

    learning tools

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,1 +1,7 @@
-from langchain import PromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langchain.agents import initialize_agent, Tool, AgentType
+
+
+def lookup(name: str) -> str:
+    return "Linkedin Profile URL"
--- third_parties/__init__.py
+++ third_parties/__init__.py
@@ -1,10 +1,11 @@
 import os
 import requests
 
+
 def scrape_linkedin_profile(str):
     """
     scrape info from LinkedIn profiles, manually scrape the information from the Linkedin profiles
     """
-    api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-    header_dic = {'Authorization': 'Bearer ' + api_key}
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": "Bearer " + api_key}
     pass
